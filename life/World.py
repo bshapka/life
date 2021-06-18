5c3fab429f3a7c009df767ed52e37da447decd6f
Brant Shapka <bshapka@gmail.com>
2021-06-17 18:51:43 -0700

    implemented World constructor and stub for __validate_state

--- life/World.py
+++ life/World.py
@@ -1,2 +1,21 @@
+from typing import List
+
 class World:
-    pass
+
+    def __init__(self, initial_state: List[List[bool]]):
+        """
+        Instantiates a World
+
+        :raises TypeError if starting_state contains a non-bool
+
+        :raises ValueError if starting_state is a jagged array
+
+        :param starting_state: represents an initial state of the World
+
+        :returns a new World
+        """
+        self.__validate_state(initial_state)
+        self.state = initial_state
+
+    def __validate_state(self, state: List[List[bool]]):
+        pass
