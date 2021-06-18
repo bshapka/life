a17f76e49f29a5502404ab44f53f160ec1301d08
Brant Shapka <bshapka@gmail.com>
2021-06-17 19:01:16 -0700

    implemented get_state

--- life/World.py
+++ life/World.py
@@ -18,4 +18,36 @@ class World:
         self.state = initial_state
 
     def __validate_state(self, state: List[List[bool]]):
-        pass
+        """
+        Validates a given state
+
+        :raises TypeError if starting_state contains a non-bool
+
+        :raises ValueError if starting_state is a jagged array
+
+        :param state: represents a state of the World
+
+        :returns void
+        """
+        row_lengths = set()
+        for row in state:
+            row_length = 0
+            for cell in row:
+                row_length += 1
+                if type(cell) is not bool:
+                    raise TypeError(
+                        "Argument state must be of type List[List[bool]].")
+            if row_lengths != set() and row_length not in row_lengths:
+                raise ValueError("Argument must not be a jagged array")
+            else:
+                row_lengths.add(row_length)
+
+    def get_state(self) -> List[List[bool]]:
+        """
+        returns state
+
+        :param none
+
+        :returns state
+        """
+        return self.state
