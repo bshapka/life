e6ae80530ffebe38784c477e311779d522f21f28
Brant Shapka <bshapka@gmail.com>
2021-06-20 11:14:15 -0700

    implemented constructor and stubs for validate_cell_size and get_random_state

--- life/Game.py
+++ life/Game.py
@@ -1,2 +1,28 @@
+from typing import List
+
+from life.World import World
+
+
 class Game:
-    pass
+    def __init__(self, cell_size: int, initial_state: List[List[bool]] = None):
+        """
+        Instantiates a Game
+
+        :param cell_size: the size of each cell in pixels
+
+        :param initial_state: an initial state of the World
+
+        :returns a new World
+        """
+        self.__validate_cell_size(cell_size)
+        if initial_state is not None:
+            self.world = World(initial_state)
+        else:
+            random_state = self.__get_random_state()
+            self.world = World(random_state)
+
+    def __validate_cell_size(self, cell_size: int):
+        pass
+
+    def __get_random_state(self):
+        pass
