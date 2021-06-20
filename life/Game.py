6c5480b77ca112f8b11c3b8c3629e5d3e2abc914
Brant Shapka <bshapka@gmail.com>
2021-06-20 13:24:58 -0700

    implemented validate_state

--- life/Game.py
+++ life/Game.py
@@ -48,15 +48,35 @@ class Game:
         :returns None
         """
         if cell_size < 0:
-            raise ValueError("Argument cell_size cannot be negative.")
+            raise ValueError("The cell_size cannot be negative.")
 
         screen_dimensions = Game.__get_screen_dimensions()
         cell_is_too_big = any(d for d in screen_dimensions if cell_size > d)
         if cell_is_too_big:
-            raise ValueError("Argument cell_size must not exceed any of the screen's dimensions.")
+            raise ValueError("The cell_size must not exceed any of the screen's dimensions.")
 
-    def __validate_state(self, state: List[List[bool]], cell_size: int):
-        pass
+    @staticmethod
+    def __validate_state(state: List[List[bool]], cell_size: int) -> None:
+        """
+        Validates a given state and cell_size combination
+
+        :raises ValueError if state dimensions scaled by cell_size are too big for the screen
+
+        :param state: an initial state for a World
+
+        :param cell_size: the size of each cell in pixels
+
+        :returns None
+        """
+        screen_width, screen_height = Game.__get_screen_dimensions()
+        state_dimensions = World(state).get_dimensions()
+        scaled_state_length, scaled_state_width = (dim * cell_size for dim in state_dimensions)
+
+        if scaled_state_length > screen_width:
+            raise ValueError("The product of the length of the state and cell_size cannot exceed the screen's width")
+
+        if scaled_state_width > screen_height:
+            raise ValueError("The product of the width of the state and cell_size cannot exceed the screen's height")
 
     def __get_random_state(self):
         pass
