018cd7b07bf6d92151510191162544890dc5a4eb
Brant Shapka <bshapka@gmail.com>
2021-06-20 12:05:34 -0700

    extracted screen dimension logic to static method get_screen_dimensions

--- life/Game.py
+++ life/Game.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import pygame
 
@@ -25,6 +25,18 @@ class Game:
             self.world = World(random_state)
 
     @staticmethod
+    def __get_screen_dimensions() -> Tuple[int, int]:
+        """
+        Returns screen dimensions in pixels as an ordered pair of form (width, height)
+
+        :returns screen dimensions in pixels as an ordered pair of form (width, height)
+        """
+        pygame.init()
+        dimensions = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+        pygame.quit()
+        return dimensions
+
+    @staticmethod
     def __validate_cell_size(cell_size: int):
         """
         Validates a given cell_size
@@ -38,9 +50,7 @@ class Game:
         if cell_size < 0:
             raise ValueError("Argument cell_size cannot be negative.")
 
-        pygame.init()
-        screen_dimensions = (pygame.display.Info().current_w, pygame.display.Info().current_h)
-        pygame.quit()
+        screen_dimensions = Game.__get_screen_dimensions()
         cell_is_too_big = any(d for d in screen_dimensions if cell_size > d)
         if cell_is_too_big:
             raise ValueError("Argument cell_size must not exceed any of the screen's dimensions.")
