4eb66546d87e9149eac41d120d9eb53d8350f2f7
Brant Shapka <bshapka@gmail.com>
2021-06-20 19:05:51 -0700

    added TestGameInvalidCellSizes

---
+++ life/test/TestGameInvalidCellSizes.py
@@ -0,0 +1,27 @@
+import pygame
+import pytest
+
+from life.Game import Game
+
+
+class TestGameInvalidCellSizes():
+    def test_cell_size_is_negative(self):
+        with pytest.raises(ValueError):
+            cell_size, initial_state = -1, []
+            Game(cell_size, initial_state)
+
+    def test_cell_size_is_too_long(self):
+        pygame.init()
+        screen_width = pygame.display.Info().current_w
+        pygame.quit()
+        with pytest.raises(ValueError):
+            cell_size, initial_state = screen_width + 1, []
+            Game(cell_size, initial_state)
+
+    def test_cell_size_is_too_wide(self):
+        pygame.init()
+        screen_height = pygame.display.Info().current_h
+        pygame.quit()
+        with pytest.raises(ValueError):
+            cell_size, initial_state = screen_height + 1, []
+            Game(cell_size, initial_state)
