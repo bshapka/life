96c8ae65bd5a945ee5eeab7c7f5e17f3203af707
Brant Shapka <bshapka@gmail.com>
2021-06-23 19:06:07 -0700

    updated Game test classes as a precursor to changes to Game class

--- life/test/TestGameInvalidCellSizes.py
+++ life/test/TestGameInvalidCellSizes.py
@@ -7,7 +7,7 @@ from life.Game import Game
 class TestGameInvalidCellSizes():
     def test_cell_size_is_negative(self):
         with pytest.raises(ValueError):
-            cell_size, initial_state = -1, []
+            cell_size, initial_state = -1, set()
             Game(cell_size, initial_state)
 
     def test_cell_size_is_too_long(self):
@@ -15,7 +15,7 @@ class TestGameInvalidCellSizes():
         screen_width = pygame.display.Info().current_w
         pygame.quit()
         with pytest.raises(ValueError):
-            cell_size, initial_state = screen_width + 1, []
+            cell_size, initial_state = screen_width + 1, set()
             Game(cell_size, initial_state)
 
     def test_cell_size_is_too_wide(self):
@@ -23,5 +23,5 @@ class TestGameInvalidCellSizes():
         screen_height = pygame.display.Info().current_h
         pygame.quit()
         with pytest.raises(ValueError):
-            cell_size, initial_state = screen_height + 1, []
+            cell_size, initial_state = screen_height + 1, set()
             Game(cell_size, initial_state)
--- life/test/TestGameInvalidStartingStates.py
+++ life/test/TestGameInvalidStartingStates.py
@@ -5,18 +5,18 @@ from life.Game import Game
 
 
 class TestGameInvalidStartingStates():
-    def test_state_is_too_long(self):
+    def test_state_exceeds_horizontal_bound(self):
         pygame.init()
         screen_width = pygame.display.Info().current_w
         pygame.quit()
         with pytest.raises(ValueError):
-            cell_size, initial_state = 1, [[False] * (screen_width + 1)]
+            cell_size, initial_state = 1, {(screen_width + 1, 0)}
             Game(cell_size, initial_state)
 
-    def test_state_is_too_wide(self):
+    def test_state_exceeds_vertical_bound(self):
         pygame.init()
         screen_height = pygame.display.Info().current_h
         pygame.quit()
         with pytest.raises(ValueError):
-            cell_size, initial_state = 1, [[False] for i in range(screen_height + 1)]
+            cell_size, initial_state = 1, {(0, screen_height + 1)}
             Game(cell_size, initial_state)
