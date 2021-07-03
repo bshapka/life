6413ab5ebcca682d7327de992ae3124da26fd2f7
Brant Shapka <bshapka@gmail.com>
2021-07-03 15:19:09 -0700

    added class comment to coordinate.py

--- life/coordinate.py
+++ life/coordinate.py
@@ -1,4 +1,13 @@
 from typing import NamedTuple
 
+"""
+represents an ordered pair in the xy plane
 
+fields:
+    x: int
+        represents the x-coordinate in the ordered pair
+        
+    y: int
+        represents the y-coordinate in the ordered pair
+"""
 Coordinate = NamedTuple("Coordinate", [("x", int), ("y", int)])
